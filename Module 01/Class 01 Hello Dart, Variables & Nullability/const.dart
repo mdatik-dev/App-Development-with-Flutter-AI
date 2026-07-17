a3411a6b7295235e43863a9341a5986fe8bb8c65
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-18 02:16:55 +0600

    Add files via upload

---
+++ Module 01/Class 01 Hello Dart, Variables & Nullability/const.dart
@@ -0,0 +1,16 @@
+void main() {
+  const a = 10;
+  const b = 10.5;
+
+  //a = 20; // Error: Can't assign to the const variable 'a'.
+  //b = 20.5; // Error: Can't assign to the const variable 'b'.
+
+  print("The value of a is = $a");
+  print("The value of b is = $b");
+
+  const pi = 3.1416;
+  print("The value of pi is = $pi");
+
+  const appName = 'Ostad';
+  print("The value of appName is = $appName");
+}
