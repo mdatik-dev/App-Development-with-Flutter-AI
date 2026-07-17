0f8584aa8c9be24dbe0ef31dbd2816fec9574af8
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-18 02:17:32 +0600

    Add files via upload

---
+++ Module 01/Class 01 Hello Dart, Variables & Nullability/double.dart
@@ -0,0 +1,28 @@
+void main() {
+  double a = 10.5;
+  double b = 20.5;
+
+  print("The value of a is = $a");
+  print("The value of b is = $b");
+
+  double sum = a + b;
+  print("The Sum of a and b is = $sum");
+
+  print(a.round());
+  print(b.round());
+
+  print(a.floor());
+  print(b.floor());
+
+  print(a.ceil());
+  print(b.ceil());
+
+  print(a.toInt());
+  print(b.toInt());
+
+  print(a.toString());
+  print(b.toString());
+
+  print(a.toStringAsFixed(3));
+  print(b.toStringAsFixed(5));
+}
