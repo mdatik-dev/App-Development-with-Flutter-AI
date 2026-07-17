920a405b285289a400539f713e12601130b8f5a0
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-18 02:18:00 +0600

    Add files via upload

---
+++ Module 01/Class 01 Hello Dart, Variables & Nullability/dynamic dataType.dart
@@ -0,0 +1,16 @@
+void main() {
+  /*
+    var name = "Engr Md Atikur Rahman";
+    name = 100;
+    print(name);
+  */
+
+  int number = 100;
+  //number = "My name is Atik"; // not accessible because of int data type
+  number = 500;
+  print(number);
+
+  dynamic data = 10;
+  data = "Hello Dart"; // this is accessible because of dynamic data type
+  print(data);
+}
