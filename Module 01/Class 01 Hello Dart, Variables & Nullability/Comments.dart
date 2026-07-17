8495ac04e47419651e4210c6a7c78483529b637c
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-18 02:16:27 +0600

    Add files via upload

---
+++ Module 01/Class 01 Hello Dart, Variables & Nullability/Comments.dart
@@ -0,0 +1,15 @@
+void main() {
+  // This is a single line comment
+  print("This is a single line comment");
+
+  /*
+    This
+    is
+    a
+    comment
+  */
+  print("This the Multi line comment");
+
+  ////// Documentation Comment
+  /// This function prints a message.
+}
