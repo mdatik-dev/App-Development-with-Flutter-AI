a23aab6d51bd21436d2bf86716539f9c087d8aab
Engr Md Atikur Rahman <mdatikurrahman.dev@gmail.com>
2026-07-18 02:18:26 +0600

    Add files via upload

---
+++ Module 01/Class 01 Hello Dart, Variables & Nullability/Explicit Type.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Atik";
+  int age = 22;
+  double cgpa = 3.90;
+  bool isStudent = true;
+
+  print("The Name is = $name");
+  print("The Age is = $age");
+  print("The CGPA is = $cgpa");
+  print("Is Student = $isStudent");
+}
